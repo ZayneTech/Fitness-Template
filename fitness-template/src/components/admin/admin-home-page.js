10ac6e55268a5ab0de822b565a0e2f3059cccc54
DylanMoreau <48781842+DylanMoreau@users.noreply.github.com>
2021-05-17 16:18:42 -0400

    Redesign of Admin Home

--- fitness-template/src/components/admin/admin-home-page.js
+++ fitness-template/src/components/admin/admin-home-page.js
@@ -24,20 +24,95 @@ const AddPage = () => {
             {blogForm ? <NewBlog setBlogForm={setBlogForm} /> : '' }
 
 
-            {/* <div className="adminHome adminHome--programs">
+            <div className="adminHome adminHome--programs">
+               <h2 className="adminHome__h2 text-center">Programs</h2>
+                <div className="adminHome__contentContainer">
 
-            </div>
+                    <div className="adminHome__content adminHome__content--padding">
+                            <p><span className="adminHome__contentSpan">Programs Uploaded:</span> 2</p>
+                    </div>
 
-            <div className="adminHome adminHome--blogs">
-                
+                    <div className="adminHome__content adminHome__content--padding">
+                        <p><span className="adminHome__contentSpan">Total Sales: </span> 243</p>
+                        <p><span className="adminHome__contentSpan">Total Profits: </span> $8230</p>
+                        <p>
+                            <span className="adminHome__contentSpan">Top Selling Program: </span> 
+
+                            <ul className="indent">
+                                <li><span className="adminHome__contentSpan">Name: </span> Calisthenics</li>
+                                <li><span className="adminHome__contentSpan">Copies Sold: </span> 173</li>
+                                <li><span className="adminHome__contentSpan">Profit: </span> $6740</li>
+                            </ul>
+                        </p>
+                    </div>
+               </div>
+               
+                <div className="adminHome__content adminHome__container">
+                        <div className="adminHome__actions">
+                            <Plus />
+                            <p>Add Program</p>
+                        </div>
+                        <div className="adminHome__actions">
+                            <Plus />
+                            <p>Edit Program</p>
+                        </div>
+                    </div>
+               </div>
+            
+
+            <div className="adminHome adminHome--blogs ">
+                <h2 className="adminHome__h2 text-center">Blogs</h2>
+
+
+                <div className="adminHome__contentContainer">
+                    <div className="adminHome__content adminHome__content--padding">
+                        <p><span className="adminHome__contentSpan">Blogs Uploaded</span>: 9</p>
+                    </div>
+
+                    <div className="adminHome__content adminHome__content--padding">
+                        <p><span className="adminHome__contentSpan">Most Viewed:</span> Gym's During Covid-19</p>
+                        <p><span className="adminHome__contentSpan">Number of Views:</span> 512</p>
+                    </div>
+
+               </div>
+            
+            
+                    <div className="adminHome__content adminHome__container">
+                        <div className="adminHome__actions">
+                            <Plus />
+                            <p>Add Blog</p>
+                        </div>
+                        <div className="adminHome__actions">
+                            <Plus />
+                            <p>Edit Blog</p>
+                        </div>
+                    </div>
             </div>
 
             <div className="adminHome adminHome--admins">
+                <h2 className=" adminHome__h2 text-center">Admins</h2>
+
+                <div className="adminHome__contentContainer">
+                    <div className="adminHome__content adminHome__content--padding">
+                        <p><span className="adminHome__contentSpan">Super Admin:</span> Dylan Moreau</p>
+                    </div>
+
+                    <div className="adminHome__content adminHome__content--padding">
+                        <p><span className="adminHome__contentSpan">Number of Admins:</span> 4</p>
+                    </div>
+                </div>
+
+                    <div className="adminHome__content adminHome__container">
+                        <div className="adminHome__actions">
+                            <Plus />
+                            <p>Add Admin</p>
+                        </div>
+                    </div>
                 
-            </div> */}
+            </div>
             
 
-            <div className="admin-add light" tabIndex="0" onClick={() => openModal(setProgramForm)}>
+            {/* <div className="admin-add light" tabIndex="0" onClick={() => openModal(setProgramForm)}>
                 <Plus />
                 <p>Add New Program</p>
             </div>
@@ -50,7 +125,7 @@ const AddPage = () => {
             <div className="admin-add light" tabIndex="0">
                 <Plus />
                 <p>Add New Admin</p>
-            </div>
+            </div> */}
         </section>
     )
 }
